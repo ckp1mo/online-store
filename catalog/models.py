ea178fd276873c90ef147e7587048596964f972b
Alexandr Gritsunov <ickpimo@gmail.com>
2023-12-03 21:09:17 +0300

    two models for DB, Category and Product

---
+++ catalog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    decription = models.TextField(**NULLABLE, verbose_name='Описание')
+    # created_at = models.DateField(**NULLABLE, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    image = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Превью')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за штуку')
+    created_at = models.DateField(**NULLABLE, verbose_name='Дата создания')
+    last_change_at = models.DateField(**NULLABLE, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
