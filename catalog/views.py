67225b434e610682dc9b1ece4e626dfe74653a83
Alexandr Gritsunov <ickpimo@gmail.com>
2023-12-05 20:44:10 +0300

    controller for products

--- catalog/views.py
+++ catalog/views.py
@@ -13,3 +13,14 @@ def home(request):
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def products(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': product_item.name,
+        'description': product_item.description,
+        'price': product_item.price,
+    }
+    return render(request, 'catalog/products.html', context)
