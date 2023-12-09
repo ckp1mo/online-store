74772d6072af467e1e64fbf3c833fcd0db75d992
Alexandr Gritsunov <ickpimo@gmail.com>
2023-12-09 19:24:54 +0300

    changed controller type from FBV to CVB

--- catalog/views.py
+++ catalog/views.py
@@ -3,23 +3,33 @@ from django.shortcuts import render
 from catalog.models import Product
 
 
-def home(request):
-    context = {
-        'object_list': Product.objects.all(),
-        'title': 'Наши продукты',
-    }
-    return render(request, 'catalog/home.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/products.html'
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
 
 
-def products(request, pk):
-    product_item = Product.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(category_id=pk),
-        'title': product_item.name,
-        'product': product_item,
-    }
-    return render(request, 'catalog/products.html', context)
+# def products(request, pk):
+#     product_item = Product.objects.get(pk=pk)
+#     context = {
+#         'object_list': Product.objects.filter(category_id=pk),
+#         'title': product_item.name,
+#         'product': product_item,
+#     }
+#     return render(request, 'catalog/products.html', context)
+
+
+# def home(request):
+#     context = {
+#         'object_list': Product.objects.all(),
+#         'title': 'Наши продукты',
+#     }
+#     return render(request, 'catalog/home.html', context)
