6b11d014ab0a0439bd8d4d20ebd5dfda2a8fbeac
Alexandr Gritsunov <ickpimo@gmail.com>
2023-12-11 13:17:00 +0300

    deleted comment

--- catalog/views.py
+++ catalog/views.py
@@ -16,21 +16,3 @@ class ProductDetailView(DetailView):
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
-
-
-# def products(request, pk):
-#     product_item = Product.objects.get(pk=pk)
-#     context = {
-#         'object_list': Product.objects.filter(category_id=pk),
-#         'title': product_item.name,
-#         'product': product_item,
-#     }
-#     return render(request, 'catalog/products.html', context)
-
-
-# def home(request):
-#     context = {
-#         'object_list': Product.objects.all(),
-#         'title': 'Наши продукты',
-#     }
-#     return render(request, 'catalog/home.html', context)
