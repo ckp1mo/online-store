a33c4f188419617789db97240c9b3c289bde4611
Alexandr Gritsunov <ickpimo@gmail.com>
2023-12-24 22:38:22 +0300

    fixed field name

--- catalog/models.py
+++ catalog/models.py
@@ -5,7 +5,7 @@ NULLABLE = {'null': True, 'blank': True}
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
-    decription = models.TextField(**NULLABLE, verbose_name='Описание')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
 
     def __str__(self):
         return f'{self.name}'
