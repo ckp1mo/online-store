7239ad89a32eb9131f33a41f03d24b8331d1ca59
Alexandr Gritsunov <ickpimo@gmail.com>
2023-12-24 22:37:29 +0300

    added forms.py

---
+++ catalog/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+
+from catalog.models import Product, Version
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def clean_name(self):
+        bad_word = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        cleaned_data = self.cleaned_data['name']
+
+        if cleaned_data in bad_word:
+            raise forms.ValidationError('Недопустимое имя продукта')
+        return cleaned_data
+
+    def clean_description(self):
+        bad_word = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        cleaned_data = self.cleaned_data['description']
+        if cleaned_data in bad_word:
+            raise forms.ValidationError('Недопустимое описание продукта')
+        return cleaned_data
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Version
+        fields = '__all__'
+
