18539196b4230b81cc9b19ec75efda8556dba72f
Alexandr Gritsunov <ickpimo@gmail.com>
2024-01-03 15:55:11 +0300

    settings for authorization and sending emails

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'slonslonsky@yandex.ru'
+EMAIL_HOST_PASSWORD = 'cybawwvqflwqnocd'
+EMAIL_USE_SSL = True
