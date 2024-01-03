34ccabb569c71e400f800f584ca3c2cb761f7565
Alexandr Gritsunov <ickpimo@gmail.com>
2024-01-03 15:51:08 +0300

    new fields "user" in BlogRecord

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from catalog.models import NULLABLE
@@ -12,10 +13,12 @@ class BlogRecord(models.Model):
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     view_count = models.IntegerField(default=0, verbose_name='Просмотры')
 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE,
+                             verbose_name='Пользователь')
+
     def __str__(self):
         return self.title
 
     class Meta:
         verbose_name = 'запись'
         verbose_name_plural = 'записи'
-
