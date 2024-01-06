fc5de4e7f9645a4ccd9d313e67f3880e9a82e5b0
Alexandr Gritsunov <ickpimo@gmail.com>
2024-01-06 23:21:39 +0300

    cached controller

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,9 +10,8 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('contacts/', contacts, name='contacts'),
     path('', ProductListView.as_view(), name='list_product'),
-    path('create/', ProductCreateView.as_view(), name='create_product'),
-    path('view/<int:pk>', ProductDetailView.as_view(), name='view_product'),
     path('create/', never_cache(ProductCreateView.as_view()), name='create_product'),
+    path('view/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name='view_product'),
     path('update/<int:pk>', ProductUpdateView.as_view(), name='update_product'),
     path('delete/<int:pk>', ProductDeleteView.as_view(), name='delete_product'),
 ]
