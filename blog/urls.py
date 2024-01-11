6800931081d95c78d63d9d901af34ca5b2ef0a85
Alexandr Gritsunov <ickpimo@gmail.com>
2024-01-12 00:30:29 +0300

    controller caching

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.decorators.cache import never_cache
+from django.views.decorators.cache import never_cache, cache_page
 
 from blog.apps import BlogConfig
 from blog.views import BlogRecordCreateView, BlogRecordListView, BlogRecordDetailView, BlogRecordUpdateView, \
@@ -10,7 +10,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('create/', never_cache(BlogRecordCreateView.as_view()), name='create_post'),
     path('', BlogRecordListView.as_view(), name='list_post'),
-    path('view/<int:pk>/', BlogRecordDetailView.as_view(), name='view_post'),
+    path('view/<int:pk>/', cache_page(60)(BlogRecordDetailView.as_view()), name='view_post'),
     path('edit/<int:pk>/', BlogRecordUpdateView.as_view(), name='update_post'),
     path('delete/<int:pk>/', BlogRecordDeleteView.as_view(), name='delete_post'),
     path('status/<int:pk>/', change_status, name='change_status'),
