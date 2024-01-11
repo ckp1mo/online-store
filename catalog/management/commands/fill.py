6a58353902107bee7266e61b5a6687a87cf4c670
Alexandr Gritsunov <ickpimo@gmail.com>
2024-01-12 00:24:58 +0300

    useless

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -3,21 +3,21 @@ from django.core.management import BaseCommand
 from catalog.models import Category
 
 
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        categories_list = [
-            {'name': 'Смартфоны', 'decription': ''},
-            {'name': 'Смарт-часы и браслеты', 'decription': ''},
-            {'name': 'Сотовые телефоны', 'decription': ''},
-        ]
-
-        # for category in categories:
-        #     Category.objects.create(**category)
-
-        Category.objects.all().delete()
-
-        categories_for_create = []
-        for categories_item in categories_list:
-            categories_for_create.append(Category(**categories_item))
-        Category.objects.bulk_create(categories_for_create)
+# class Command(BaseCommand):
+#
+#     def handle(self, *args, **options):
+#         categories_list = [
+#             {'name': 'Смартфоны', 'decription': ''},
+#             {'name': 'Смарт-часы и браслеты', 'decription': ''},
+#             {'name': 'Сотовые телефоны', 'decription': ''},
+#         ]
+#
+#         # for category in categories:
+#         #     Category.objects.create(**category)
+#
+#         Category.objects.all().delete()
+#
+#         categories_for_create = []
+#         for categories_item in categories_list:
+#             categories_for_create.append(Category(**categories_item))
+#         Category.objects.bulk_create(categories_for_create)
