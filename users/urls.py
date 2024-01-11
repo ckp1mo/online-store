85a344a8223c3032fd502275b35ac8a23a10e7b9
Alexandr Gritsunov <ickpimo@gmail.com>
2024-01-12 00:30:31 +0300

    controller caching

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import RegisterView, UserUpdateView, generate_new_password, VerificationView
+from users.views import RegisterView, UserUpdateView, generate_new_password, VerificationView, \
+    error_verification
 
 app_name = UsersConfig.name
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('verification/', VerificationView.as_view(), name='verification'),
+    path('verification/verify_error/', error_verification, name='verify_email_error'),
     path('profile/', UserUpdateView.as_view(), name='profile'),
     path('profile/generate_new_password', generate_new_password, name='generate_new_password'),
 ]
